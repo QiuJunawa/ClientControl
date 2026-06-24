dedf7457e74d14bf6b0ec01585b8adaf26526e0e
QiuJunawa <3790909954@qq.com>
2026-06-24 11:55:02 +0800

    fix: correct Mixin import and method injection

--- src/main/java/com/qiujunawa/clientcontrol/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/qiujunawa/clientcontrol/mixin/ClientPlayerEntityMixin.java
@@ -3,19 +3,15 @@ package com.qiujunawa.clientcontrol.mixin;
 import com.qiujunawa.clientcontrol.client.ClientControlClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
-public abstract class ClientPlayerEntityMixin {
+public class ClientPlayerEntityMixin {
 
-	@Shadow
-	public abstract void updateVelocity();
-
-	@Inject(method = "updateVelocity", at = @At("HEAD"), cancellable = true)
-	private void onUpdateVelocity(CallbackInfo ci) {
+	@Inject(method = "tickMovement", at = @At("HEAD"))
+	private void onTickMovement(CallbackInfo ci) {
 		float multiplier = ClientControlClient.getSpeedMultiplier();
 		if (multiplier < 1.0f && multiplier > 0.0f) {
 			ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
